df67c9638959fe83ebb6c3b66efb46f4595740be
Dennis <76845672+DennisNeu@users.noreply.github.com>
2026-01-31 20:36:37 +0100

    model

--- goldwatch/gold/models.py
+++ goldwatch/gold/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class GoldPurchase(models.Model):
-    amount_in_grams = models.DecimalField(decimal_places=3)
+    amount_in_grams = models.DecimalField(decimal_places=3, max_digits=10)
     date = models.DateTimeField("date purchased")
-    total_price_eur = models.DecimalField(decimal_places=2)
+    total_price_eur = models.DecimalField(decimal_places=2, max_digits=10)

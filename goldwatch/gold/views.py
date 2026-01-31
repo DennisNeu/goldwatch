37dacc773a0d46f28ec54de0a5549a6d71343842
Dennis <76845672+DennisNeu@users.noreply.github.com>
2026-01-31 18:33:55 +0100

    API

---
+++ goldwatch/gold/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from key import key
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("I track gold")
+
+def make_gapi_request():
+    api_key = key
+    symbol = "XAU"
+    curr = "USD"
+    date = ""
+
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}{date}"
+    
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        result = response.text
+        print(result)
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
+
+make_gapi_request()
